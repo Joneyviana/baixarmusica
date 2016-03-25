6240f67041a9830b89c39ac2d3e33a33c716bff4
Joneyviana <arcistiel@gmail.com>
2016-03-25 18:02:01 -0300

    recuperando o projeto

--- app/apis/ view_header_request.py
+++
@@ -1,3 +0,0 @@
-import urllib2
-opener = urllib2.build_opener(urllib2.HTTPHandler(debuglevel=1))
-request = opener.open('https://www.google.com.br/?q=name')
--- config/application.rb
+++ config/application.rb
@@ -1,34 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Escola
+module Tubinho
   class Application < Rails::Application
-
-    config.generators do |g|
-      g.test_framework :rspec,
-        fixtures: true,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false,
-        controller_specs: true,
-        request_specs: false
-      g.fixture_replacement :factory_girl, dir: "spec/factories"
-    end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -42,7 +21,6 @@ module Escola
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
