a46d56a60faa19b79d7213de3d56cf1011299118
Joneyviana <arcistiel@gmail.com>
2016-03-25 13:22:36 -0300

    modificando posição javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,19 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
+
+$(window).load(function(){
+
+
+  $("button.Search").click(function(){
+    var query = $("#q").val();
+    $.get( "/search", { q:query } )
+      .done(function( data ) {
+        var result = HandlebarsTemplates['videos/teste'](data);
+        $(".container-list").html(result)
+    });
+  
+  });
+
+});
+
